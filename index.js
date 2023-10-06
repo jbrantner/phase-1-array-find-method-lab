cbcb5aca9683486ac22d66aaa1b83bcbd9392596
Jordan Brantner <thejordanbrantner@gmail.com>
2023-10-06 15:28:49 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(array) {
+    let findWin = array.find(function(object){return object.result === "W"})
+    if(findWin === undefined) {
+        return undefined;
+    } else {
+    return findWin.year;}
+}
+
